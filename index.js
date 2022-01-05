5236211eefe49684bef31507859e5bc61a62e038
andrewyoo93 <andrew.yoo93@gmail.com>
2022-01-05 16:51:52 -0500

    Added packages required for application, added questions, and added inquirer to init function

--- index.js
+++ index.js
@@ -1,13 +1,63 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: 'input',
+    message: 'What is the name of your project?',
+    name: 'title',
+  },
+  {
+    type: 'input',
+    message: 'Write a description for your project.',
+    name: 'description',
+  },
+  {
+    type: 'input',
+    message: 'Write a user story for your project.',
+    name: 'userStory',
+  },
+  {
+    type: 'input',
+    message: 'Write the installation instructions for your project. (Note: Generated README will already include the instructions to clone the repo.)',
+    name: 'installation',
+  },
+  {
+    type: 'input',
+    message: 'Write a description of your planned web visual. (Note: After README is generated, please go back to edit the visual alt tag and visual link.)',
+    name: 'visual',
+  },
+  {
+    type: 'input',
+    message: 'Write any contributors to your project and their contact information.',
+    name: 'credits',
+  },
+  {
+    type: 'input',
+    message: 'Write a list of features for your project. (Note: Features must be written as an array. e.g. ["Feature 1", "Feature 2", "Feature 3"])',
+    name: 'features',
+  },
+  {
+    type: 'input',
+    message: 'Write how other developers can contact you to contribute to the project.',
+    name: 'contact',
+  },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  console.log(fileName)
+  console.log(data)
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer
+    .prompt(questions)
+    .then((response) => module.exports = response)
+}
 
 // Function call to initialize app
 init();
