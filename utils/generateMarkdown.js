1fec88ac6d63e1e97ca3c982e279eb495c54ef2b
andrewyoo93 <andrew.yoo93@gmail.com>
2022-01-06 16:57:14 -0500

    Added an additional line break in the renderUserStory function, added a function to render the additional installation instructions, added an additional line break in the renderCreditsSection function, changed the renderLicenseSection function to include a description of what license the application is covered under, adjusted format of new and previous callbacks to make sure they do not leave a blank line break if the questions returned an empty string

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,10 @@
-const renderUserStory = userStory => (userStory != '') ? '\n## User Story\n\`\`\`\n' + userStory + '\n\`\`\`' : ''
+const renderUserStory = userStory => (userStory != '') ? '\n\n## User Story\n\`\`\`\n' + userStory + '\n\`\`\`' : ''
+
+const renderAdditionalInstructions = installation => (installation != '') ? '\n' + installation : ''
 
 const renderCreditsLink = credits => (credits != '') ? '\n- [Credits](#credits)' : ''
 
-const renderCreditsSection = credits => (credits != '') ? '\n## Credits\n' + credits : ''
+const renderCreditsSection = credits => (credits != '') ? '\n\n## Credits\n' + credits : ''
 
 const renderLicenseBadge = license => (license != '') ? '[![license](https://img.shields.io/badge/license-' + license + '-blue)](./LICENSE)' : ''
 
@@ -11,7 +13,7 @@ const renderLicenseLink = license => (license != '') ? '\n- [License](#license)'
 
 // TODO: Add change the link so that the user can choose a badge color once a badge color question is added to the index.js
 // Returns license section header and badge. If there is no license, return an empty string.
-const renderLicenseSection = license => (license != '') ? '\n\n## License\n[![license](https://img.shields.io/badge/license-' + license + '-blue)](./LICENSE)' : ''
+const renderLicenseSection = license => (license != '') ? '\n\n## License\n[![license](https://img.shields.io/badge/license-' + license + '-blue)](./LICENSE) This application is covered under the ' + license + ' license.' : ''
 
 const renderFeaturesLink = features => (features != '') ? '\n- [Features](#features)' : ''
 
@@ -30,8 +32,7 @@ const generateMarkdown = data => {
   return `# ${data.title}
 ${renderLicenseBadge(data.license)}
 ## Description
-${data.description}
-${renderUserStory(data.userStory)}
+${data.description}${renderUserStory(data.userStory)}
 
 ## Table of Contents
 - [Installation](#installation)
@@ -42,13 +43,11 @@ ${renderUserStory(data.userStory)}
 To get the app on your local machine:
 Click on the code button on the repo.
 Copy the HTTPS or SSH link.
-Run the command \`git clone\` with the copied HTTPS or SSH link.
-${data.installation}
+Run the command \`git clone\` with the copied HTTPS or SSH link.${renderAdditionalInstructions(data.installation)}
 
 ## Deployed Web Visual
 ${data.visual}
-![alt text](enter committed web visual file path here)
-${renderCreditsSection(data.credits)}${renderLicenseSection(data.license)}${renderFeaturesList(data.features)}
+![alt text](enter committed web visual file path here)${renderCreditsSection(data.credits)}${renderLicenseSection(data.license)}${renderFeaturesList(data.features)}
 
 ## How to Contribute
 ${data.contact}
