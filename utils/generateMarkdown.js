cbece5f0efa1b802a487269b0987d1973dd4d6c3
andrewyoo93 <andrew.yoo93@gmail.com>
2022-01-06 15:53:22 -0500

    Moved render license badge to be included in the render license section function, added function to render the feature list, and moved calls in the template literal so blank spaces are not included if the function returns an empty string

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -4,17 +4,22 @@ const renderCreditsLink = credits => (credits != '') ? '\n- [Credits](#credits)'
 
 const renderCreditsSection = credits => (credits != '') ? '\n## Credits\n' + credits : ''
 
-// TODO: Add change the link so that the user can choose a badge color once a badge color question is added to the index.js
-// Creates a license badge that links to the license file in the repo. If there is no license, return an empty string.
-const renderLicenseBadge = license => (license != '') ? '[![license](https://img.shields.io/badge/license-' + license + '-blue)](./LICENSE)' : '';
-
 // Returns the license table of contents link. If there is no license, returns an empty string.
 const renderLicenseLink = license => (license != '') ? '\n- [License](#license)' : ''
 
-// Returns license section header. If there is no license, return an empty string
-const renderLicenseSection = license => (license != '') ? '\n## License' : ''
+// TODO: Add change the link so that the user can choose a badge color once a badge color question is added to the index.js
+// Returns license section header and badge. If there is no license, return an empty string.
+const renderLicenseSection = license => (license != '') ? '\n\n## License\n[![license](https://img.shields.io/badge/license-' + license + '-blue)](./LICENSE)' : ''
 
-const renderFeaturesList
+const renderFeaturesList = features => {
+  if (features != '') {
+    const featurePrep = '\n\n## Features\n- ' + features;
+    const featureList = featurePrep.replaceAll(' & ', '\n- ');
+    return featureList;
+  } else {
+    return '';
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 const generateMarkdown = data => {
@@ -39,11 +44,7 @@ ${data.installation}
 ## Deployed Web Visual
 ${data.visual}
 ![alt text](enter committed web visual file path here)
-${renderCreditsSection(data.credits)}${renderLicenseSection(data.license)}${renderLicenseBadge(data.license)}
-
-
-## Features
-${renderFeaturesList(data.features)}
+${renderCreditsSection(data.credits)}${renderLicenseSection(data.license)}${renderFeaturesList(data.features)}
 
 ## How to Contribute
 ${data.contact}
@@ -52,7 +53,3 @@ ${data.contact}
 }
 
 module.exports = { generateMarkdown };
-
-
-// Features code
-// ${renderFeatureList(data.features)}
